6227aa8ad17decaa953d374c394073060d5f13fe
Achu Yakub <aygyun_94@outlook.de>
2024-06-11 21:57:19 +0200

    feat(server): initialize Express server with middleware and error handling
    - Set up Express application.
    - Initialize middleware for JSON parsing and HTTP request logging.
    - Configure server to listen on the specified port.
    - Add error handling for server startup.
    - Import and configure settings from environment variables.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,32 @@
 // server.ts
+import morgan from "morgan";
+import express, { type Express } from "express";
 
 // Init db
 import "./config/sequelize";
+// Import config from .env
+import config, { type Config } from "./config/config";
+
+// Server port
+const PORT = config.server_port;
+
+// Express app init.
+const app: Express = express();
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Middleware for logging HTTP requests
+app.use(morgan('dev'));
+
+// Start server, handle error.
+(function startServer() {
+    // Running the server
+    const server = app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
+
+    server.on('error', (err: any) => {
+        console.error('Failed to start server:', err);
+    });
+})();
